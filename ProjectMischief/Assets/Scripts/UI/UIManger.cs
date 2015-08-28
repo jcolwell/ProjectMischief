8d257c49ecba69a78183fc5382711dccc03516c8
coleunites <coleunites@shaw.ca>
2015-08-27 17:29:49 -0700

    [UI]  - created UI Scene for correction  - stubbed UI for correction

---
+++ ProjectMischief/Assets/Scripts/UI/UIManger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManger : MonoBehaviour 
+{
+
+
+
+	public void CloseUI()
+	{
+		Debug.Log ("Cosing UI");
+		Destroy(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
