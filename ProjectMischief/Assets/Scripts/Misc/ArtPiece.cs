2447f92cc1fdc88c4bcf49f1c1c82b22325f42d0
coleunites <coleunites@shaw.ca>
2015-09-10 14:28:41 -0700

    [ui]  - ui fix

---
+++ ProjectMischief/Assets/Scripts/Misc/ArtPiece.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArtPiece : MonoBehaviour {
+
+	public string [] paintingchoices = new string[3];
+	public string [] yearChoices= new string[3];
+	public string[] artistChoices= new string[3];
+	public string[] correctChoices = new string[3];
+	public string[] currentChoices = new string[3];
+
+	void OnTriggerEnter(Collider other)
+	{
+		Application.LoadLevelAdditive ("UITest");
+
+		UIManger uiManger = GameObject.Find ("UIManger").GetComponent<UIManger> ();
+
+		uiManger.paintingchoices = paintingchoices;
+		uiManger.yearChoices = yearChoices;
+		uiManger.artistChoices = artistChoices;
+		uiManger.correctChoices = correctChoices;
+		uiManger.currentPainting.text = currentChoices[0];
+		uiManger.currentYear.text = currentChoices[1];
+		uiManger.currentArtist.text = currentChoices[2];
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
