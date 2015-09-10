697c805bd1244abdcea145ce988dd600019fddde
coleunites <coleunites@shaw.ca>
2015-09-10 06:14:59 -0700

    [UI]  - added correction Menu Script  - added functionality to correction menu

---
+++ ProjectMischief/Assets/Scripts/UI/CorrectionMenu.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+enum CorrectionFieldTypes
+{
+	eCorrectionChoice1,
+	eCorrectionChoice2,
+	eCorrectionChoice3,
+	eCurrentField,
+	eCurrentChoice,
+	eMAXTYPES
+};
+
+public class CorrectionMenu : MonoBehaviour 
+{
+	public UIManger uiManger;
+
+	public Text [] fields = new Text[(int)CorrectionFieldTypes.eMAXTYPES];
+	Text uiManCurChoice = null;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		fields [(int)CorrectionFieldTypes.eCurrentField] = GameObject.Find ("CurrentField").GetComponent<Text> ();
+		fields [(int)CorrectionFieldTypes.eCurrentChoice] = GameObject.Find ("CurrentChoice").GetComponent<Text> ();
+		fields [(int)CorrectionFieldTypes.eCorrectionChoice1] = GameObject.Find ("CorrectionInfo1").GetComponent<Text> ();
+		fields [(int)CorrectionFieldTypes.eCorrectionChoice2] = GameObject.Find ("CorrectionInfo2").GetComponent<Text> ();
+		fields [(int)CorrectionFieldTypes.eCorrectionChoice3] = GameObject.Find ("CorrectionInfo3").GetComponent<Text> ();
+	}
+
+	public void CorrectPainting()
+	{
+		fields [(int)CorrectionFieldTypes.eCurrentField].text = "Painting";
+		fields [(int)CorrectionFieldTypes.eCurrentChoice].text = uiManger.currentPainting.text;
+		uiManCurChoice = uiManger.currentPainting;
+		for(int i = 0 ; i <= (int)CorrectionFieldTypes.eCorrectionChoice3; ++i)
+		{
+			fields[i].text = uiManger.paintingchoices[i];
+		}
+	}
+
+	public void CorrectYear()
+	{
+		fields [(int)CorrectionFieldTypes.eCurrentField].text = "Year";
+		fields [(int)CorrectionFieldTypes.eCurrentChoice].text = uiManger.currentYear.text;
+		uiManCurChoice = uiManger.currentYear;
+		for(int i = 0 ; i <= (int)CorrectionFieldTypes.eCorrectionChoice3; ++i)
+		{
+			fields[i].text = uiManger.yearChoices[i];
+		}
+	}
+
+	public void CorrectArtist()
+	{
+		fields [(int)CorrectionFieldTypes.eCurrentField].text = "Artist";
+		fields [(int)CorrectionFieldTypes.eCurrentChoice].text = uiManger.currentArtist.text;
+		uiManCurChoice = uiManger.currentArtist;
+		for(int i = 0 ; i <= (int)CorrectionFieldTypes.eCorrectionChoice3; ++i)
+		{
+			fields[i].text = uiManger.artistChoices[i];
+		}
+	}
+
+	public void SubmitChoice(int choice)
+	{
+		uiManCurChoice.text = fields [choice].text;
+		uiManCurChoice = null;
+	}
+}
--- ProjectMischief/Assets/Scripts/UI/UIManger.cs
+++ ProjectMischief/Assets/Scripts/UI/UIManger.cs
@@ -1,26 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UIManger : MonoBehaviour 
 {
+	public string [] paintingchoices = new string[3];
+	public string [] yearChoices= new string[3];
+	public string[] artistChoices= new string[3];
+	public string[] correctChoices = new string[3];
 
-
+	[HideInInspector]
+	public Text currentPainting;
+	[HideInInspector]
+	public Text currentYear;
+	[HideInInspector]
+	public Text currentArtist;
+	
 	public void CloseUI()
 	{
-		Debug.Log ("Cosing UI");
 		Destroy(this.gameObject);
 	}
 
-	// Use this for initialization
-	void Start () 
+	void Awake () 
 	{
-	
+		currentPainting = GameObject.Find ("PaintingChoice").GetComponent<Text> ();
+		currentYear = GameObject.Find ("YearChoice").GetComponent<Text> ();
+		currentArtist = GameObject.Find ("ArtistChoice").GetComponent<Text> ();
 	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	}
-
-
 }
