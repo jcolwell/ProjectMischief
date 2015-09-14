49de3b8e7bd95e1829eaf5e18d32688d71bf9c68
coleunites <coleunites@shaw.ca>
2015-09-14 16:30:22 -0700

    [UI]  - fixed issue with ui correction menu not loading properly

--- ProjectMischief/Assets/Scripts/Misc/ArtPiece.cs
+++ ProjectMischief/Assets/Scripts/Misc/ArtPiece.cs
@@ -12,26 +12,32 @@ public class ArtPiece : MonoBehaviour {
 	[HideInInspector]
 	public bool playerIsInRange = false;
 
+
 	bool openingMenu = false;
+	// counts how many times Update() has been called since LoadMenu() has been called
+	int currentTick = 0;
+
 	public void LoadMenu()
 	{
 		Application.LoadLevelAdditive ("UITest");
-
 		openingMenu = true;
+		currentTick = 0;
 	}
 
 	void Update()
 	{
-		if( openingMenu == true)
+		//currentTick is checked to make sure that the uimanager has been loaded
+		if( openingMenu == true && currentTick > 0)
 		{
 			openingMenu = false;
-			UIManger uiManger = GameObject.Find ("UIManger").GetComponent<UIManger> ();
+			GameObject uiMangerGameObject = GameObject.Find ("UIManger");
+			UIManger uiManger = uiMangerGameObject.GetComponent<UIManger> ();
 			uiManger.currentPainting.text = currentChoices[0];
 			uiManger.currentYear.text = currentChoices[1];
 			uiManger.currentArtist.text = currentChoices[2];
 			uiManger.artPiece = gameObject.GetComponent<ArtPiece> ();
 		}
-	}
 
-	// Use this for initialization
+		++currentTick;
+	}
 }
