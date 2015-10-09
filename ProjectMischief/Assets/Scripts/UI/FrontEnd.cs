3ac80bc63a76786518c627d6adb855b9f8dda42a
coleunites <coleunites@shaw.ca>
2015-10-09 12:37:02 -0700

    [level2]  - made button go to front end
    [icon]
     - added icon

---
+++ ProjectMischief/Assets/Scripts/UI/FrontEnd.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrontEnd : MonoBehaviour 
+{
+    public void Level1()
+    {
+        Application.LoadLevel( 2 );
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+	
+}
---
+++ ProjectMischief/Assets/Scripts/UI/QuitToMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitToMenu : MonoBehaviour 
+{
+    public void ToMenu()
+    {
+        Application.LoadLevel( 0 );
+    }
+}
