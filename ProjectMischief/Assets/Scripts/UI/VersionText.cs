513eed52127b79bb87fc44324e4f1d16b5ef441f
JoshColwell <jcolwell4@me.com>
2015-10-30 08:57:15 -0700

    [FrontEnd] - added version text build referencing. [Prefabs] attached missing scripts to Camera Hazard

---
+++ ProjectMischief/Assets/Scripts/UI/VersionText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VersionText : MonoBehaviour
+{
+    Text verNum;
+
+    // Use this for initialization
+    void Start ()
+    {
+        verNum = GetComponent<Text> ();
+        verNum.text = "Version: " + Application.version;
+    }
+}
