253a007ec4637b2ef20b66148f125498254329e7
unknown <285512@AI911LAB234P013.AIV.EDMC.STU>
2016-01-07 09:26:57 -0800

    More Shit to fix, why am i even pushing this

--- ProjectMischief/Assets/Scripts/Misc/MovementReticle.cs
+++ ProjectMischief/Assets/Scripts/Misc/MovementReticle.cs
@@ -1,52 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovementReticle : MonoBehaviour 
+public class MovementReticle : MonoBehaviour
 {
     public float lifeTime;
 
-    static MovementReticle curInstance;
-    static bool isRunning = false;
-
     float timeElapsed = 0;
-	
-	void Start () 
-    {
-        if(isRunning)
-        {
-            if( curInstance != null )
-            {
-                Destroy( curInstance.gameObject );
-                curInstance = null;
-            }
-        }
-        else
-        {
-            isRunning = true;
-        }
 
-        curInstance = this;
+    public void Reset()
+    {
         timeElapsed = 0.0f;
-	}
-	
-	void Update () 
+    }
+
+    public void Reset( Vector3 pos )
+    {
+        timeElapsed = 0.0f;
+        gameObject.transform.position = pos;
+    }
+
+    void Start()
+    {
+        timeElapsed = 0.0f;
+    }
+
+    void Update()
     {
         timeElapsed += Time.deltaTime;
-        
-        if(timeElapsed >= lifeTime)
+
+        if( timeElapsed >= lifeTime )
         {
-            isRunning = false;
-            Destroy( this.gameObject );
+            gameObject.SetActive( false );
         }
-	}
+    }
 
     void OnTriggerEnter( Collider other )
     {
-       if(other.CompareTag("Player"))
-       {
-           isRunning = false;
-           Destroy( this.gameObject );
-       }
+        if( other.CompareTag( "Player" ) )
+        {
+            gameObject.SetActive( false );
+        }
     }
 
 }
--- ProjectMischief/Assets/Scripts/UI/UIControl.cs
+++ ProjectMischief/Assets/Scripts/UI/UIControl.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class UIControl : MonoBehaviour 
+public class UIControl : MonoBehaviour
 {
     //public
     public bool pauseTimeWhenLoaded = true;
@@ -10,33 +10,64 @@ public class UIControl : MonoBehaviour
     // Protected
     protected UITypes uiType;
 
+    // private 
+    private int renderOrder = 1; // higher numbers are rendered first
+
     //public
-    public UIControl(UITypes type)
+    public UIControl( UITypes type )
     {
         uiType = type;
     }
 
-	public void CloseUI()
-	{
-        if (pauseTimeWhenLoaded)
+    public UIControl( UITypes type, int order )
+    {
+        uiType = type;
+        renderOrder = order;
+    }
+
+    public void CloseUI()
+    {
+        DurringCloseUI();
+        if( pauseTimeWhenLoaded )
         {
             Time.timeScale = 1.0f;
         }
-		Destroy(this.gameObject);
-	}
+        Destroy( this.gameObject );
+    }
+
+    public void SetCanvas()
+    {
+        GameObject canvasObject = transform.FindDeepChild( "Canvas" ).gameObject;
+        Canvas canvas = canvasObject.GetComponent<Canvas>();
+
+        SettingsData settingData = PersistentSceneData.GetPersistentData().GetSettingsData();
+
+        if( settingData.fixedAspectRatio )// place for settings check
+        {
+            canvas.renderMode = RenderMode.ScreenSpaceCamera;
+            canvas.worldCamera = Camera.main;
+            canvas.planeDistance = 1.0f;
+            canvas.sortingOrder = renderOrder;
+        }
+        else
+        {
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        }
+    }
 
     //private
-        // DO NOT OVERIDE THESE 2 FUNCTIONS IN THE CHILD CLASSES IF YOU DO DEATH ONTO YOU
-    void OnEnable() 
-	{
-        UIManager.instance.RegisterUI(gameObject, uiType);
+    // DO NOT OVERIDE THESE 2 FUNCTIONS IN THE CHILD CLASSES IF YOU DO DEATH ONTO YOU
+    void OnEnable()
+    {
+        UIManager.instance.RegisterUI( gameObject, uiType );
+        SetCanvas();
 
         DurringOnEnable();
-        if (pauseTimeWhenLoaded)
+        if( pauseTimeWhenLoaded )
         {
             Time.timeScale = 0.0f;
         }
-	}
+    }
 
     void OnDestroy()
     {
@@ -52,23 +83,27 @@ public class UIControl : MonoBehaviour
     protected virtual void DurringDestroy()
     {
     }
+
+    protected virtual void DurringCloseUI()
+    {
+    }
 }
 
 // http://answers.unity3d.com/questions/799429/transformfindstring-no-longer-finds-grandchild.html
 public static class TransformDeepChildExtension
 {
-	//Breadth-first search
-	public static Transform FindDeepChild(this Transform aParent, string aName)
-	{
-		Transform result = aParent.Find(aName);
-		if (result != null)
-			return result;
-		foreach(Transform child in aParent)
-		{
-			result = child.FindDeepChild(aName);
-			if (result != null)
-				return result;
-		}
-		return null;
-	}
+    //Breadth-first search
+    public static Transform FindDeepChild( this Transform aParent, string aName )
+    {
+        Transform result = aParent.Find( aName );
+        if( result != null )
+            return result;
+        foreach( Transform child in aParent )
+        {
+            result = child.FindDeepChild( aName );
+            if( result != null )
+                return result;
+        }
+        return null;
+    }
 }
