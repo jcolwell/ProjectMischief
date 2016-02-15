dcb97752e33fcddc44a45dd5a5440309408ff2c2
Cole Rudniski <coleunites@shaw.ca>
2016-02-15 13:53:25 -0800

    [Gameplay] - finshed coin prefab  - added script to add currncy when the object is collided with

---
+++ ProjectMischief/Assets/Scripts/Misc/Coin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour 
+{
+    public int currencyValue = 1;
+
+    void OnTriggerEnter( Collider other )
+    {
+        if(other.CompareTag("Player"))
+        {
+            UIManager.instance.IncreaseCoinsEarned(currencyValue);
+            Destroy( this.gameObject );
+        }
+    }
+}
