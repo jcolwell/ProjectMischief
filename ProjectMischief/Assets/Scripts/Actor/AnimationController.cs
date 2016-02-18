d746045037deb863314f7ce88bd03c499b034729
unknown <318000@AI911LAB234P012.AIV.EDMC.STU>
2016-02-18 12:40:16 -0800

    Playing with more animation bullshit

--- ProjectMischief/Assets/Scripts/Actor/AnimationController.cs
+++ ProjectMischief/Assets/Scripts/Actor/AnimationController.cs
@@ -1,16 +1,98 @@
-﻿using UnityEngine;
+﻿//======================================================
+// File: AnimationController.cs
+// Description:    Animation stuffs
+//======================================================
+
+//======================================================
+// Includes
+//======================================================
+using UnityEngine;
 using System.Collections;
 
+//======================================================
 public class AnimationController : MonoBehaviour 
 {
-    Animator anim;
-    public float speed = 1.2F;
-    bool isWalking;
+    //======================================================
+    //Private
+    //======================================================
+    Animation anime;
+
+    //======================================================
+    //State
+    //======================================================
+    public enum State
+    {
+        Idle,
+        Walk,
+        Run
+    };
+
+    State state;
+
+    //======================================================
 
     void Start()
     {
-        anim = GetComponentInChildren<Animator>();
-        anim.Play("idle");
+        anime = this.GetComponentInChildren<Animation>();
+        state = State.Idle;
     }
 
+    //======================================================
+
+    void Update()
+    {
+        switch(state)
+        {
+        case State.Idle:
+        {
+            UpdateIdle();
+        }
+        break;
+        case State.Walk:
+        {
+            UpdateWalk();
+        }
+        break;
+        case State.Run:
+        {
+            UpdateRun();
+        }
+        break;
+        }
+    }
+
+    //======================================================
+
+    void UpdateIdle()
+    {
+       anime.Play( "idle" );
+       print( "ITS FUCKING IDLE" );
+    }
+
+    //======================================================
+
+    void UpdateWalk()
+    {
+        anime.Play( "walk" );
+    }
+
+    //======================================================
+
+    void UpdateRun()
+    {
+        anime.Play( "run" );
+    }
+
+    //======================================================
+
+    public void ChangeState(State s)
+    {
+        if(s != state)
+        {
+            anime.Stop();
+            state = s;
+        }
+    }
+
+    //======================================================
 }
