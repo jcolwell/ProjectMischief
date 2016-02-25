9dff390885cc27639945ddc6953239bb7db3a37b
Cole Rudniski <coleunites@shaw.ca>
2016-02-25 12:16:40 -0800

    [UI]  -Update Map

---
+++ ProjectMischief/Assets/Scripts/UI/UITextScroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UITextScroll : MonoBehaviour 
+{
+    ScrollRect scrollRect;
+	// Use this for initialization
+	void Start () 
+    {
+        scrollRect = GetComponent<ScrollRect>();
+        scrollRect.verticalNormalizedPosition = 1.0f;
+    }
+	
+
+	// Update is called once per frame
+	void Update () 
+    {
+        scrollRect.verticalNormalizedPosition = 1.0f;
+	}
+}
