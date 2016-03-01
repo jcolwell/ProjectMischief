37284538f4736a485cd2bff87821e04c168c29bb
Cole Rudniski <coleunites@shaw.ca>
2016-03-01 10:03:52 -0800

    [UI] Made art images apear on the map and image token

---
+++ ProjectMischief/Assets/Scripts/UI/ImageToken.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImageToken : MonoBehaviour 
+{
+    public Image painting;
+    public Image tokenImage;
+
+    public SpriteRenderer painting3D;
+    public SpriteRenderer imageToken3D;
+}
