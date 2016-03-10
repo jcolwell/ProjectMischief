9a226f8258edfcffc754f81c813993c7ab3a4b41
Cole Rudniski <coleunites@shaw.ca>
2016-03-10 11:02:02 -0800

    [UI] finished loading screen

--- ProjectMischief/Assets/Scripts/UI/LoadingScreen.cs
+++ ProjectMischief/Assets/Scripts/UI/LoadingScreen.cs
@@ -1,29 +1,45 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class LoadingScreen :MonoBehaviour
 {
     public float lifeTimeAfterLevelLoaded = 0.5f;
+    public float crossfadeTime = 1.0f;
+    public Image loadingScreenImage;
+    public Image nextLoadingScreenImage;
+    public Image backGround;
+    bool isCrossFading = false;
+    bool isDoneCrossfading = false;
 
     bool levelLoaded = false;
-    float timeElpased = 0.0f;
+    public float timeElpased = 0.0f;
     float timeWhenLoaded = 0.0f;
     float deltaTime = 0.0f;
     float lastFramesTime = 0.0f;
+    float tIncrement;
 
+    Color curLoadingScreenColor;
+    Color nextLoadingScreenColor;
+    Color backGroundColor;
     void Start()
     {
         DontDestroyOnLoad(this);
         GameObject canvasObject = transform.FindDeepChild("Canvas").gameObject;
         Canvas canvas = canvasObject.GetComponent<Canvas>();
         canvas.sortingOrder = 6;
+
+        curLoadingScreenColor = loadingScreenImage.color;
+        nextLoadingScreenColor = nextLoadingScreenImage.color;
+        backGroundColor = backGround.color;
     }
 
     public void LevelLoaded()
     {
         levelLoaded = true;
-        timeWhenLoaded = timeElpased;
+        timeElpased = 0.0f;
         lastFramesTime = Time.realtimeSinceStartup;
+        tIncrement = 1 / crossfadeTime;
     }
 
     void Update()
@@ -31,9 +47,36 @@ public class LoadingScreen :MonoBehaviour
         CalculateDeltaTime();
         timeElpased += deltaTime;
 
-        if (levelLoaded && timeElpased >= lifeTimeAfterLevelLoaded + timeWhenLoaded)
+        
+
+        if(isCrossFading)
         {
-            Destroy(gameObject);
+            if(timeElpased > crossfadeTime)
+            {
+                isCrossFading = false;
+                isDoneCrossfading = true;
+                timeElpased = lifeTimeAfterLevelLoaded;
+            }
+            curLoadingScreenColor.a -= tIncrement * deltaTime;
+            nextLoadingScreenColor.a += tIncrement * deltaTime;
+            loadingScreenImage.color = curLoadingScreenColor;
+            nextLoadingScreenImage.color = nextLoadingScreenColor;
+        }
+        else if (levelLoaded && timeElpased >= (lifeTimeAfterLevelLoaded * 2))
+        {
+            nextLoadingScreenColor.a -= tIncrement * deltaTime;
+            nextLoadingScreenImage.color = nextLoadingScreenColor;
+            backGroundColor.a -= tIncrement * deltaTime;
+            backGround.color = backGroundColor;
+            if( timeElpased >= (lifeTimeAfterLevelLoaded * 2) + crossfadeTime )
+            {
+                Destroy( gameObject );
+            }
+        }
+        else if( (levelLoaded && timeElpased >= lifeTimeAfterLevelLoaded) && (!isCrossFading && !isDoneCrossfading) )
+        {
+            isCrossFading = true;
+            timeElpased = 0.0f;
         }
     }
 
