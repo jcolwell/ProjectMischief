aed2746e80c707037efd668ae12c5e347f08deee
Cole Rudniski <coleunites@shaw.ca>
2016-09-06 12:28:11 -0700

    [UI] - changed how version number is displayed

--- ProjectMischief/Assets/Scripts/UI/VersionText.cs
+++ ProjectMischief/Assets/Scripts/UI/VersionText.cs
@@ -5,11 +5,12 @@ using System.Collections;
 public class VersionText : MonoBehaviour
 {
     Text verNum;
-
+    public string versionNumber;
     // Use this for initialization
     void Start ()
     {
         verNum = GetComponent<Text> ();
-        verNum.text = "Version: " + Application.version;
+        //verNum.text = "Version: " + Application.version;
+        verNum.text = "Version: " + versionNumber; 
     }
 }
